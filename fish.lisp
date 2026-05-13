7aba3a7c4eef63a60d6edf5692044ad05d410bfd
Mufaro J. Machaya <mail@moofy.org>
2026-05-12 17:19:08 -0700

    fih

--- fish.lisp
+++ fish.lisp
@@ -2,9 +2,11 @@
 ;; This is equivalent to `import numpy as np`
 (ql:quickload :numcl)
 (ql:quickload :alexandria)
+(ql:quickload :cl-opengl) ;; for plotting, equivalent to matplotlib
 (defpackage :fish
   (:use :cl)
-  (:local-nicknames (:nc :numcl)))
+  (:local-nicknames (:nc :numcl))
+  (:export :simulate :orientation-vector))
 (in-package :fish)
 
 ;; First, we build the fish model based on the specificaitons.
@@ -22,6 +24,7 @@
 
 ;; The number of fish to model in this school
 (defparameter *num-fish* 20)
+(defparameter *initial-generation-bounds* (nc:asarray '(10 10 10)))
 
 ;; Fish Model
 
@@ -31,75 +34,175 @@
 (defparameter *cs-area* (* 4 pi (expt *fish-length* 2)))
 (defparameter *self-propelled-speed* (/ *volumetric-flow-rate* *cs-area*))
 
+;; Linear Algebra Functions
+
+(defun dot (a b)
+  "Dot Product"
+  (declare (type nc:array a b))
+  (nc:sum (nc:* a b)))
+
+(defun norm (v)
+  "Distance vector calculation"
+  (declare (type nc:array v))
+  (nc:sqrt (dot v v)))
+
 ;; Simulation Calculation Functions
 
 (defun half-length-vector (fish)
   "The distance vector from the center-of-mass to the front or back"
+  (declare (type fish fish))
   (nc:* 1/2 *fish-length* (fish-orientation fish)))
 
 (defun source-pos (fish)
   "The front of the fish, x_f,i"
+  (declare (type fish fish))
   (nc:+ (fish-position fish)
 	(half-length-vector fish)))
 
 (defun sink-pos (fish)
   "The back of the fish, x_b,i"
-  (nc:- (fish-position)
+  (declare (type fish fish))
+  (nc:- (fish-position fish)
 	(half-length-vector fish)))
-
-(defun norm (v)
-  "Distance vector calculation"
-  (nc:sqrt (nc:dot v v)))
 
 (defun compute-decrease-vector (a b)
   "Same thing as computing the vector representation of 1/r^2"
-  ((let ((distance-vec (nc:- a b)))
-     (nc:/ distance-vec (expt (norm distance-vec) 3)))))
+  (declare (type nc:array a b))
+  (let ((distance-vec (nc:- a b)))
+     (nc:/ distance-vec (expt (norm distance-vec) 3))))
 
 (defun source-decrease-vector (fish-a fish-b)
   "Computes the decrease vector 1/r^2 between the sources and sinks"
+  (declare (type fish fish-a fish-b))
   (nc:-
    (compute-decrease-vector (source-pos fish-a) (source-pos fish-b))
    (compute-decrease-vector (source-pos fish-a) (sink-pos   fish-b))))
 
 (defun sink-decrease-vector (fish-a fish-b)
   "Computes the decrease vector 1/r^2 for computing the sink velocity"
+  (declare (type fish fish-a fish-b))
   (nc:-
    (compute-decrease-vector (sink-pos fish-a) (source-pos fish-b))
    (compute-decrease-vector (sink-pos fish-a) (sink-pos   fish-b))))
 
-(defun compute-velocity (fish other-fishes contibution-fn)
+(defun compute-velocity (fish other-fishes contribution-fn)
   "Computes the total contribution"
-  (let ((compute-contibution (alexandria:curry #'contribution-fn fish))
-	(total-distance-contributions
-	  (nc:sum (nc:asarray (mapcar #'compute-contribution other-fishes)))))
+  (declare (type fish     fish)
+	   (type vector   other-fishes)
+	   (type function contribution-fn))
+  (let* ((compute-contribution
+	   (alexandria:curry contribution-fn fish))
+	 (total-distance-contributions
+	   (nc:sum (nc:asarray
+		    (map 'vector compute-contribution other-fishes)))))
     (nc:* *self-propelled-speed*
 	  (nc:+ (fish-orientation fish)
 		(nc:* *fish-length* total-distance-contributions)))))
 
 (defun source-vel (fish other-fishes)
   "Computes the source velocity for a given fish"
-  (compute-velocity fish other-fishes source-decrease-vector))
+  (declare (type fish fish)
+	   (type vector other-fishes))
+  (compute-velocity fish other-fishes #'source-decrease-vector))
 
 (defun sink-vel (fish other-fishes)
   "Computes the sink velocity for a given fish"
-  (compute-velocity fish other-fishes sink-decrease-vector))
+  (declare (type fish fish)
+	   (type vector other-fishes))
+  (compute-velocity fish other-fishes #'sink-decrease-vector))
 
 (defun translational-vel (fish other-fishes)
   "Computes the translational velocity for a given fish"
-  (nc:/ (nc:+ (source-vel fish other-fishes) (sink-vel fish other-fishes)) 2))
+  (declare (type fish fish)
+	   (type vector other-fishes))
+  (nc:/ (nc:+ (source-vel fish other-fishes)
+	      (sink-vel fish other-fishes))
+	2))
 
 (defun rotational-vel (fish other-fishes)
   "Computes the rotational velocity for a given fish"
-  (let ((relative-vel (nc:- (source-vel fish other-fishes)
-			    (sink-vel fish other-fishes)))
+  (declare (type fish fish)
+	   (type vector other-fishes))
+  (let* ((relative-vel
+	   (nc:- (source-vel fish other-fishes)
+		 (sink-vel fish other-fishes)))
 	(lagrange-mult
-	  (nc:* -1/2 (nc:dot relative-vel (fish-orientation fish)))))
+	  (nc:* -1/2 (dot relative-vel (fish-orientation fish)))))
     (nc:/
      (nc:+ relative-vel
 	   (nc:* 2 lagrange-mult (fish-orientation fish)))
      *fish-length*)))
 
-;; Simulation Function
+;; Simulation Functions
 
-(defun simulate (initial-state stop-time time-step) nil)
+;; Theta [0, 2PI) and Phi [0,PI)
+(defparameter *angle-bounds* (vector (* 2 pi) pi))
+(defun generate-random-fish ()
+  "Computes a random fish with a random heading direction"
+  (make-fish :position (map 'vector #'random *initial-generation-bounds*)
+	     :orientation (map 'vector #'random *angle-bounds*)))
+
+(defun generate-random-distribution (n)
+  "Generates n random autofish within a maximum starting bounds"
+  (declare (type fixnum n))
+  (make-array n :initial-contents
+	      (loop :for i :below n
+		    :collect (generate-random-fish))))
+
+(defparameter *orientation-vector-length* 2)
+(defun spherical->cartesian (theta phi)
+  "Converts the spherical orientation to a cartesian unit vector"
+  (nc:asarray (vector
+	       (* (sin phi) (cos theta))
+	       (* (sin phi) (sin theta))
+	       (cos phi))))
+
+(defun orientation-vector (fish)
+  "Generates the orientation vector for a given fish"
+  (declare (type fish fish))
+  (nc:* *orientation-vector-length*
+	(apply #'spherical->cartesian (fish-orientation fish))))
+
+(defun display-state (state)
+  "Displays the state in a three-dimensional plot"
+  (declare (type vector state))
+  (gl:clear :color-buffer-git :depth-buffer-bit)
+
+  (loop :for fish :across state :do
+    ;; Display the fish as a point
+    (gl:begin :points)
+    (apply #'gl:vertex (fish-position fish))
+    (gl:end)
+
+    ;; Display the orientation as a small line
+    (gl:begin :lines)
+    (apply #'gl:vertex (fish-position fish))
+    (apply #'gl:vertex (nc:+ (fish-position fish)
+			     (orientation-vector fish)))
+    (gl:end))
+  
+  (gl:flush)
+  (swap-buffers)
+  (sleep 1/60)) ; 60 FPS
+
+(defun calculate-next-state (state time-step use-barnes-hut?)
+  "Computes the next state based on the previous state and the time-step"
+  (declare (type vector state)
+	   (type number time-step))
+  (loop :for fish :across state :do
+    (let ((other-fish (remove-fish fish state)))
+      (if use-barnes-hut?
+	  (setf other-fish (barnes-hut-simplify other-fish fish)))
+      ())))
+
+(defun simulate (stop-time time-step use-barnes-hut?)
+  "The main simulation function"
+  (declare (type number stop-time time-step)
+	   (type bool   use-barnes-hut?))
+  (let ((time 0)
+	(state (generate-random-distribution *num-fish*)))
+    (loop :while (< time stop-time)
+	  :do (display-state state)
+	      (setf state (calculate-next-state state time-step
+						use-barnes-hut?))
+	      (incf time time-step))))
