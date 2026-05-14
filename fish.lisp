1bdf7bf565a4840cbb5549dc815fdbfd8216913c
Mufaro <sandmanmufmaf@gmail.com>
2026-05-13 20:02:01 -0700

    barnes hut sorta

--- fish.lisp
+++ fish.lisp
@@ -25,7 +25,8 @@
 
 ;; The number of fish to model in this school
 (defparameter *num-fish* 20)
-(defparameter *initial-generation-bounds* (nc:asarray '(10 10 10)))
+(defparameter *initial-generation-box-length* 10)
+(defparameter *barnes-hut-threshold* 1/4) ; theta
 
 ;; Fish Model
 
@@ -138,6 +139,9 @@
 
 ;; Theta [0, 2PI) and Phi [0,PI)
 (defparameter *angle-bounds* (vector (* 2 pi) pi))
+(defparameter *initial-generation-bounds*
+  (nc:asarray (make-array 3 :initial-element *initial-generation-box-length*)))
+
 (defun generate-random-fish ()
   "Computes a random fish with a random heading direction"
   (make-fish :position (map 'vector #'random *initial-generation-bounds*)
@@ -189,22 +193,93 @@
 
 ;; State Update Functions
 
-(defun generate-barnes-hut-octree (state)
-  "Generates the Barnes-Hut octree according to
-   'The Barnes Hut Algorithm' by Tom Ventimiglia and Kevin Wayne"
-  (declare (optimize (safety 3) (speed 0) (debug 1)))
-  (declare (type (list fish) state))
+(defstruct octree-node
+  (center (nc:/ *initial-generation-bounds* 2) :type nc:array)
+  (half-size (/ *initial-generation-box-length* 2) :type number)
+  (children nil :type (vector octree-node 8))
+  (object nil :type fish))
 
-  ;; Recursive generator function
-  (labels ((recurse-octree-gen (tree ) ))
-    
-    (recurse-octree-gen  (first state) (rest state))))
+(defun octree-get-child-index (node next)
+  "Obtains the `child index` of the fish, or the octant to place it in"
+  (declare (type octree-node node)
+	   (type fish next))
+  (let* ((center (octree-node-center node))
+	 (pos (fish-position next))
+	 (x-bit (if > (aref pos 0) (aref center 0)) 1 0)
+	 (y-bit (if > (aref pos 1) (aref center 1)) 2 0)
+	 (z-bit (if > (aref pos 2) (aref center 2)) 4 0))
+    (+ x-bit y-bit z-bit)))
+
+(defun octree-insert (node next)
+  "Inserts a fish into the octree"
+  (declare (type octree-node node)
+	   (type fish next))
+  (let ((children (octree-node-children node))
+	(index (child-index node next)))
+    (if children (octree-insert (aref children index) next)
+	(setf (octree-node-object node)
+	      (make-octree-node :center (fish-position next)
+				:half-size (/ (octree-node-half-size node) 2)
+				:object next)))))
+
+(defun generate-barnes-hut-octree (state)
+  "Generates an octree representation of the state"
+  (declare (optimize (safety 3) (speed 0))
+	   (type (list fish) state))
+  (labels ((recurse-octree-gen (tree remaining)
+	     (declare (type list tree remaining))
+	     (if (null remaining) tree
+		 (recurse-octree-gen
+		  (octree-insert tree (first remaining))
+		  (rest remaining)))))
+    (recurse-octree-gen (make-octree-node) state)))
+
+(defun barnes-hut-simplify (node fish)
+  "Produces a list of simplified fish interactions based on the Octree."
+  (declare (type octree-node node)
+	   (type fish fish))
+  ;; Nodes are too far if the *fish-length* / distance < *barnes-hut-threshold* 
+  (flet ((node-too-far? (node fish)
+	   (declare (type octree-node node)
+		    (type fish fish)
+		    (optimize (speed 3) (safety 0)))
+	   (let ((distance (norm (nc:- (octree-node-center node) (fish-pos fish)))))
+	     (< (/ *fish-length* distance)
+		*barnes-hut-threshold*))))
+    (cond (((null node) nil)
+	   ;; The node is too far away, so cluster the tree
+	   (node-too-far? node fish)
+	   (list (octree->cluster node)))
+	  
+	  ;; Leaf, so return the exact fish
+	  ((null (octree-node-children node))
+	   (octree-node-object node))
+
+	  ;; Otherwise, return this fish and recurse across all children
+	  (t (append (octree-node-object node)
+		     (loop :for child :across (octree-node-children node) :append
+			   (barnes-hut-simplify child fish))))))))
 
 (defun calculate-next-state-barnes-hut (state time-step)
   "Calculates the next state using Barnes-Hut approximation"
   (declare (type vector state) (type number time-step))
-  (let ((octree (generate-barnes-hut-octree (coerce 'list state)))))
-    ()))
+  (let ((octree (generate-barnes-hut-octree (coerce 'list state))))
+    (loop :for fish :across state :collect
+	  ;; Perform a Barnes-Hut simplification to the octree
+	  (let* ((other-fish-approximated (coerce (barnes-hut-simplify octree fish) 'vector))
+
+		 ;; Compute translational and rotational velocity
+		 (trans-vel (translational-vel fish other-fish-approximated))
+		 (rot-vel (rotational-vel fish other-fish-approximated))
+
+		 ;; Compute the change in translational position and orientation
+		 (pos-delta (nc:* trans-vel time-step))
+		 (orientation-delta (nc:* rot-vel time-step)))
+
+	    ;; Produce next fish
+	    (make-fish
+	     (nc:+ (fish-position fish) pos-delta)
+	     (nc:+ (fish-orientation fish) orientation-delta))))))
 
 (defun calculate-next-state-iterative (state time-step)
   "Calculates the next state using standard iteration"
@@ -217,12 +292,11 @@
 					(subseq state 0 i)
 					(subseq state (1+ i)))
 	:collect
-	
 	;; Compute the Translational Velocity
-	(let* ((trans-vel (translational-vel fish other-fishes))
+	(let* ((trans-vel (translational-vel fish other-fish))
 	       
 	       ;; Compute the rotational velocity
-	       (rot-vel (rotational-vel fish other-fishes))
+	       (rot-vel (rotational-vel fish other-fish))
 
 	       ;; Compute the change in translational position
 	       (pos-delta (nc:* trans-vel time-step))
